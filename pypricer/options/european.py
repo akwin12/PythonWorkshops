da726701e0e2ca06727805f8e5c8d49694ed561c
akwin12 <sz.badura@interia.pl>
2024-06-05 17:59:38 +0200

    PUT OPP

--- pypricer/options/european.py
+++ pypricer/options/european.py
@@ -19,9 +19,18 @@ class EuropeanOption:
         """Abstract method"""
         raise NotImplementedError("Payoff needs to be implemented in child classes")
 
-    def price(self, path: pd.DataFrame, rfr: float) -> float:
-        """Abstract method"""
-        raise NotImplementedError("Pricing needs to be implemented in child classes")
+    def price(self, path: pd.DataFrame, rfr: float) -> np.ndarray:
+        """Compute the price of the option using the Black-Scholes formula.
+
+        Args:
+            path (pd.DataFrame): DataFrame with asset price realizations.
+                Columns represent different trajectories, rows represent time steps.
+            rfr (float): Risk-free rate.
+
+        Returns:
+            np.ndarray: Array containing option prices for each trajectory.
+        """
+        return  np.exp(-self.expiry * rfr) * self.payoff(path=path).mean()
 
 
 class EuropeanCallOption(EuropeanOption):
@@ -39,18 +48,7 @@ class EuropeanCallOption(EuropeanOption):
         """
         return np.maximum(0.0, path.iloc[-1, :] - self.strike_price)
 
-    def price(self, path: pd.DataFrame, rfr: float) -> np.ndarray:
-        """Compute the price of the option using the Black-Scholes formula.
-
-        Args:
-            path (pd.DataFrame): DataFrame with asset price realizations.
-                Columns represent different trajectories, rows represent time steps.
-            rfr (float): Risk-free rate.
-
-        Returns:
-            np.ndarray: Array containing option prices for each trajectory.
-        """
-        return np.exp(-self.expiry * rfr) * self.payoff(path=path).mean()
+    
 
 class EuropeanPutOption(EuropeanOption):
     def payoff(self, path: pd.DataFrame) -> np.ndarray:
@@ -65,18 +63,7 @@ class EuropeanPutOption(EuropeanOption):
         """
         return np.maximum(0.0, self.strike_price - path.iloc[-1, :] )
 
-    def price(self, path: pd.DataFrame, rfr: float) -> np.ndarray:
-        """Compute the price of the option using the Black-Scholes formula.
-
-        Args:
-            path (pd.DataFrame): DataFrame with asset price realizations.
-                Columns represent different trajectories, rows represent time steps.
-            rfr (float): Risk-free rate.
-
-        Returns:
-            np.ndarray: Array containing option prices for each trajectory.
-        """
-        return  np.exp(-self.expiry * rfr) * self.payoff(path=path).mean()
+   
 
 
 if __name__ == "__main__":
